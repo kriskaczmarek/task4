db50a2e8212fb8df11cdf5e7a95dee11e2102719
Krzysztof Kaczmarek <1krzysztof.kaczmarek@gmail.com>
2023-01-10 17:28:00 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'task4';
+  maxNumber: number = 3;
+  nameInput: string = '';
+  nameList: Array<string> = [];
+  currentNumber: number = this.nameList.length;
+  selectedFramework: string = 'angular';
+  frameworks: string[] = ['vue', 'angular', 'react', 'node'];
+  fullroom: boolean = true;
+
+  add() {
+    this.currentNumber++;
+    this.nameList.push(`${this.nameInput} - likes ${this.selectedFramework}`);
+    this.nameInput = '';
+    this.selectedFramework = 'angular';
+  }
+  clearBtn() {
+    this.nameList = [];
+    this.currentNumber = 0;
+  }
+  removePerson(item: string) {
+    this.nameList = this.nameList.filter((el) => el != item);
+    this.currentNumber = this.nameList.length;
+  }
+  editBtn(item: string) {
+    console.log(item);
+  }
 }
