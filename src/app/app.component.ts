58b2ad659b3095283886dbdfd738bf66c641bc73
Krzysztof Kaczmarek <1krzysztof.kaczmarek@gmail.com>
2023-01-11 10:09:16 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,41 @@
 import { Component } from '@angular/core';
 
+interface nameList {
+  name: string;
+  preference: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'task4';
-  maxNumber: number = 3;
-  nameInput: string = '';
-  nameList: Array<string> = [];
-  currentNumber: number = this.nameList.length;
-  selectedFramework: string = 'angular';
-  frameworks: string[] = ['vue', 'angular', 'react', 'node'];
-  fullroom: boolean = true;
+  public max: number = 3;
+  public name: string = '';
+  public data: Array<nameList> = [];
+  public preference: string = 'angular';
+  public frameworks: Array<string> = ['vue', 'angular', 'react', 'node'];
 
   add() {
-    this.currentNumber++;
-    this.nameList.push(`${this.nameInput} - likes ${this.selectedFramework}`);
-    this.nameInput = '';
-    this.selectedFramework = 'angular';
+    if (this.name && this.preference) {
+      this.data.push({
+        name: this.name,
+        preference: this.preference,
+      });
+    }
+    this.name = '';
+    this.preference = 'angular';
   }
   clearBtn() {
-    this.nameList = [];
-    this.currentNumber = 0;
+    this.data = [];
   }
-  removePerson(item: string) {
-    this.nameList = this.nameList.filter((el) => el != item);
-    this.currentNumber = this.nameList.length;
+  remove(item:nameList){
+    this.data = this.data.filter(el=> el !== item)
   }
-  editBtn(item: string) {
-    console.log(item);
+  edit(idx:number){
+    const item = this.data[idx]
+    this.name = item.name
+    this.preference = item.preference
+    this.remove(item)
   }
 }
